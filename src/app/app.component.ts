ee55cb65db4b3b7f738ba4343d9a1027a8fe3192
Chris Loewer <chris.loewer@gmail.com>
2017-11-27 19:13:23 -0600

    Initialize basic layout - values not wired in yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'CMS Prototype';
+  pageContent = 'Chambray hell of hammock squid. Shaman cred PBR&B glossier marfa dreamcatcher. Typewriter banh pop-up,' +
+    'hella humblebrag pok pok leggings try-hard art party stumptown dreamcatcher kombucha. Adaptogen ethical keytar,' +
+    'williamsburg schlitz gentrify farm-to-table taxidermy yuccie gochujang pok pok snackwave intelligentsia. ';
+  pageTitle = 'Lorem Hipsum';
 }
