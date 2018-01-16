4b58edaabf701c63dd1c0c48e3ccbf3b4c033fca
Chris Loewer <chris.loewer@gmail.com>
2018-01-16 12:40:19 -0600

    Pull animations into separate file

---
+++ src/app/components/post/animations.ts
@@ -0,0 +1,54 @@
+import { trigger, state, style, animate, transition, query, stagger, animateChild } from '@angular/animations';
+
+export const PostAnimations: any[] = [
+  trigger('editTrigger', [
+    state('default', style({
+      flexGrow: 1
+    })),
+    state('edit', style({
+      flexGrow: 1
+    })),
+    state('preview', style({
+      flexGrow: 0,
+      height: 0
+    })),
+    transition('preview => *', [
+      animate('200ms ease-out')
+    ]),
+    transition('* <=> *', [
+      query('@isVisible', animateChild(), {optional: true}),
+      animate('200ms ease-out')
+    ])
+  ]),
+
+  trigger('previewTrigger', [
+    state('default', style({
+      flexGrow: 1
+    })),
+    state('edit', style({
+      flexGrow: 0,
+      height: 0
+    })),
+    state('preview', style({
+      flexGrow: 1
+    })),
+    transition('edit => *', [
+      animate('200ms ease-out')
+    ]),
+    transition('* <=> *', [
+      query('@isVisible', animateChild(), {optional: true}),
+      animate('200ms ease-out')
+    ])
+  ]),
+
+  trigger('isVisible', [
+    state('true', style({
+      opacity: 1
+    })),
+    state('false', style({
+      opacity: 0,
+      overflow: 'hidden'
+    })),
+    transition('* <=> *', animate('200ms ease-out'))
+  ])
+];
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,40 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
+// import { trigger, state, style, animate, transition, query, stagger, animateChild } from '@angular/animations';
 import { Post } from '../../classes/post';
 import { PostService } from '../../services/post/post.service';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { PostAnimations } from '../post/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
   providers: [PostService],
-  animations: [
-    trigger('editTrigger', [
-      state('default', style({
-        flexGrow: 1
-      })),
-      state('edit', style({
-        flexGrow: 1
-      })),
-      state('preview', style({
-        flexGrow: 0
-      })),
-    transition('* <=> *', animate('800ms ease-out'))
-    ]),
-
-    trigger('previewTrigger', [
-      state('default', style({
-        flexGrow: 1
-      })),
-      state('edit', style({
-        flexGrow: 0
-      })),
-      state('preview', style({
-        flexGrow: 1
-      })),
-    transition('* <=> *', animate('800ms ease-out'))
-    ])
-  ]
+  animations: PostAnimations
 })
 
 export class PostComponent implements OnInit {
@@ -72,4 +47,13 @@ export class PostComponent implements OnInit {
     }
     console.log(p.mode);
   }
+
+  isEditVisible(): boolean {
+    return this.post.mode !== 'preview' ? true : false;
+  }
+
+  isPreviewVisible(): boolean {
+    return this.post.mode !== 'edit' ? true : false;
+  }
+
 }
