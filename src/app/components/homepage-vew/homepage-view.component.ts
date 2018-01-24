100da923d7ccc65f5028078058acd6936b658019
chris_loewer <cloewer@iclasspro.com>
2018-01-24 09:35:08 -0600

    Load home page from db if no info is passed through.

--- src/app/components/homepage-vew/homepage-view.component.ts
+++ src/app/components/homepage-vew/homepage-view.component.ts
@@ -1,15 +1,36 @@
-import { Component, Input } from '@angular/core/';
+import {Component, Input, OnInit} from '@angular/core/';
 import { Post } from '../../classes/post';
+import {PostService} from '../../services/post/post.service';
 
 @Component({
   selector: 'app-homepage-view',
   templateUrl: './homepage-view.component.html',
-  styleUrls: ['./homepage-view.component.scss']
+  styleUrls: ['./homepage-view.component.scss'],
+  providers: [PostService]
 })
 
-export class HomepageViewComponent {
+export class HomepageViewComponent implements OnInit {
 
   @Input() post: Post;
 
-  constructor() {}
+  constructor(private postService: PostService) {
+  }
+
+  ngOnInit() {
+    if (this.post) {
+      console.log(this.post);
+    } else {
+      console.log('No Post Input');
+      this.post = new Post();
+      this.getPost();
+    }
+  }
+
+    getPost(): void {
+      this.postService.getPost()
+        .subscribe(
+          (p) => this.post = new Post(p),
+          (err) => console.warn(err)
+        );
+    }
 }
