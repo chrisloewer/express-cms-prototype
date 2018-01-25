bfad906a3d3a0ad746c06d627ca5aec1f5525ca0
Chris Loewer <chris.loewer@gmail.com>
2018-01-24 20:41:57 -0600

    Update api path.

--- src/app/services/post/post.service.ts
+++ src/app/services/post/post.service.ts
@@ -1,30 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Post } from '../../classes/post';
 import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 
-import { HttpHeaders, HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class PostService {
 
-  private apiUrl = 'https://k6f9hug0pi.execute-api.us-west-2.amazonaws.com/test';
+  private apiUrl = 'https://k6f9hug0pi.execute-api.us-west-2.amazonaws.com/test/postapi';
 
   constructor(
     private http: HttpClient
   ) { }
 
   getPost(): Observable<Post> {
-    const postUrl = this.apiUrl + '/postapi';
+    const postUrl = this.apiUrl + '/00001';
 
     return this.http
       .get<Post>(postUrl);
   }
 
   setPost(p: Post): Observable<any> {
-    const postUrl = this.apiUrl + '/postapi';
+    const postUrl = this.apiUrl;
 
     return this.http.post(postUrl, p);
   }
