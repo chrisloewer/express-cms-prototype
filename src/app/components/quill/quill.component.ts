3052b92ab4df291f9659e9a0121ebe4f941c2913
Chris Loewer <chris.loewer@gmail.com>
2018-01-28 00:29:38 -0600

    Move default quill styles to component.

--- src/app/components/quill/quill.component.ts
+++ src/app/components/quill/quill.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-quill',
   templateUrl: './quill.component.html',
-  styleUrls: ['./quill.component.scss']
+  styleUrls: [
+    './quill-snow-theme-styles.scss',
+    './quill.component.scss']
 })
 export class QuillComponent implements OnInit {
 
