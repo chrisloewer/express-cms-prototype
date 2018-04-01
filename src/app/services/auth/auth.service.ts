93ddff42a9ab88ea0d84d324b32e53bbc6ea4a79
chris <chris.loewer@gmail.com>
2018-04-01 17:53:14 -0500

    Check if user token has expired before loading user info.

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { User } from '../../classes/user';
 import { environment } from '../../../environments/environment';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable()
 export class AuthService {
@@ -18,16 +19,27 @@ export class AuthService {
 
   static loadLocalUser(): User {
     let user: User;
+    const jwtHelper = new JwtHelperService();
+
     try {
       user = JSON.parse(localStorage.getItem('userInfo'));
+      if (!jwtHelper.isTokenExpired(user.token)) {
+        console.log('User loaded from local storage');
+        return user;
+      }
     } catch (e) {
-      user = new User();
+      console.warn('Unable to load local user info');
     }
-    return user;
+    return new User();
+  }
+
+  public static isAuthenticated(): boolean {
+    const user = AuthService.loadLocalUser();
+    return user.auth;
   }
 
   // Contact API.  If user exists, it will return JWT token
-  login(username, password): Observable<User> {
+  public login(username, password): Observable<User> {
     const loginUrl = environment.apiUrl + '/login';
     const httpOptions = {
       headers: new HttpHeaders(
@@ -38,5 +50,6 @@ export class AuthService {
     };
     return this.httpClient.post<User>(loginUrl, { content: null }, httpOptions);
   }
+
 
 }
