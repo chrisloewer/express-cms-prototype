846abdf35207470e35e80feded2f7ec1e0feb36a
chris <chris.loewer@gmail.com>
2018-04-01 19:48:29 -0500

    Add loading icon.  Return to correct page after logging in.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,25 +10,45 @@ import { AuthService } from '../../services/auth/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  username: String;
-  password: String;
+  username: string;
+  password: string;
+  errorMsg: string;
+  generalMsg: string;
+  loading = false;
+  loggedIn = false;
+  returnUrl: string;
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+    if (AuthService.isAuthenticated()) {
+      this.loggedIn = true;
+      this.generalMsg = 'User already authenticated.';
+    }
   }
 
   login(): void {
+    this.loading = true;
+    this.errorMsg = null;
     this.authService.login(this.username, this.password)
       .subscribe(
         (user) => {
-          console.log(user);
           AuthService.storeLocalUser(user);
+          this.generalMsg = 'Login successful';
+          this.loggedIn = true;
+          this.loading = false;
+          this.router.navigateByUrl(this.returnUrl);
         },
         (err) => {
-          console.log(err);
+          console.warn('Error', err);
+          this.errorMsg = 'Incorrect login credentials';
+          this.loading = false;
         }
       );
   }
