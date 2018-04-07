393e20f55b6f26b6e00620cb22a6c71b77342eef
chris <chris.loewer@gmail.com>
2018-04-07 13:37:09 -0500

    Fix issue by adding image modal gallery to entry components.

--- src/app/modules/edit-view.module.ts
+++ src/app/modules/edit-view.module.ts
@@ -37,6 +37,7 @@ import { ImageGalleryModalComponent } from '../components/image-gallery-modal/im
     ImageGalleryModalComponent,
     PageEditComponent,
     QuillComponent
-  ]
+  ],
+  entryComponents: [ImageGalleryModalComponent]
 })
 export class EditViewModule {}
